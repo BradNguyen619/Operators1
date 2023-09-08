353676ab3bd902690554f96b24d575971381b89e
Brad Nguyen <brad.nguyen@mrjaffesclass.com>
2023-09-08 11:17:19 -0700

    Finished Operators

--- Operators.java
+++ Operators.java
@@ -2,9 +2,18 @@ public class Operators
 {
     public static void main(String[] args)
     {
-        int i = 10 ;
-        int f += i ;
+        int i ;
+        double f = 2.5 ;
         boolean flag = true ;
+        i = 10 ; {
+            System.out.println(++i) ;
+        }
         
+        f += i ; {
+            System.out.println(f) ;
+        }
+        flag = !flag ; {
+            System.out.println(flag) ;
+        }
     }
 }
